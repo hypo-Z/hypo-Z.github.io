1795c9b5fd79af5afafe4c2c34722235e138a4ad
zhouhaibo <zhouhaibo@mzhcorp.com>
2022-03-23 11:17:32 +0800

    Site updated: 2022-03-23 11:17:31

--- js/matery.js
+++ js/matery.js
@@ -128,12 +128,12 @@ $(function () {
     let $nav = $('#headNav');
     let $backTop = $('.top-scroll');
     // 当页面处于文章中部的时候刷新页面，因为此时无滚动，所以需要判断位置,给导航加上绿色。
-    //showOrHideNavBg($(window).scrollTop());
+    showOrHideNavBg($(window).scrollTop());
     $(window).scroll(function () {
         /* 回到顶部按钮根据滚动条的位置的显示和隐藏.*/
         let scroll = $(window).scrollTop();
         //不显示绿色
-        //showOrHideNavBg(scroll);
+        showOrHideNavBg(scroll);
     });
 
     function showOrHideNavBg(position) {
